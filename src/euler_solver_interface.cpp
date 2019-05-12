78719ce2556c42316b0b770ffe2b7db81b2b73d9
Zymrael <mic.poli@hotmail.it>
2019-05-12 14:03:55 +0900

    feat: added rk4 cuda

--- src/euler_solver_interface.cpp
+++ src/euler_solver_interface.cpp
@@ -7,20 +7,21 @@
 #define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
+
 // CUDA declarations
 
-torch::Tensor euler_solver_cuda(torch::Tensor F, torch::Tensor x0, double dt, int steps, int W);
-
+torch::Tensor solver_cuda(torch::Tensor F, torch::Tensor x0, double dt, int steps, int W, string name);
 
 
 // C++ interface
 
-torch::Tensor eulerSolver(torch::Tensor F, torch::Tensor x0, double dt, int steps, int W){
+torch::Tensor ode_solve(torch::Tensor F, torch::Tensor x0, double dt, int steps, int W, string name){
     CHECK_INPUT(F); 
     CHECK_INPUT(x0);
-    return euler_solver_cuda(F, x0, dt, steps, W);
+
+    return solver_cuda(F, x0, dt, steps, W, name);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("eulerCUDA", &eulerSolver, "Euler Solver (CUDA)");
+  m.def("solver_cuda", &solver_cuda, "ODE Solver (CUDA)");
 }
