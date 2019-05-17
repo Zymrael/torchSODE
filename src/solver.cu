221bd6cf2baf321e4fce635f642464a5101b0f22
Michael Poli <20184659@eelab35.kaist.ac.kr>
2019-05-17 15:25:02 +0900

    epic fix #3

--- src/solver.cu
+++ src/solver.cu
@@ -50,7 +50,7 @@ general_solver(method_t method, torch::PackedTensorAccessor<float, 2> F_a, torch
 }
 
 __global__ void
-compact_diagonal_solver(method_t method, torch::PackedTensorAccessor<float, 2> F_in, torch::PackedTensorAccessor<float, 1> x0_a, torch::PackedTensorAccessor<float, 1> g_a, float dt, int steps, int x0_size) { 
+compact_diagonal_solver(method_t method, torch::PackedTensorAccessor<float, 2> F_a, torch::PackedTensorAccessor<float, 1> x0_a, torch::PackedTensorAccessor<float, 1> g_a, float dt, int steps, int x0_size) { 
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if(tid < x0_size){
         auto x0_in = x0_a[tid];
