892b4b59ad837a3f5da3428c7f6f707ce7aef0f8
Jacob Odgård Tørring <jacob.torring@gmail.com>
2019-05-17 18:20:40 +0900

    Adds dispatch macro

--- src/solver.cu
+++ src/solver.cu
@@ -43,7 +43,7 @@ rk4_method(const scalar_t F_in, scalar_t x0_in, const scalar_t g_in, const float
 
 template <typename scalar_t>
 __global__ void
-general_solver(method_t method, const Packed<scalar_t, 2> F_a, Packed<scalar_t, 1> x0_a, const Packed<scalar_t, 1> g_a, const scalar_t dt, int steps, size_t x0_size) {
+general_solver(method_t method, const Packed<scalar_t, 2> F_a, Packed<scalar_t, 1> x0_a, const Packed<scalar_t, 1> g_a, const float dt, int steps, size_t x0_size) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if(tid < x0_size){
         auto x0_in = x0_a[tid];
@@ -123,9 +123,9 @@ torch::Tensor solve_cuda(torch::Tensor F, torch::Tensor x0, torch::Tensor g, flo
 
     method_t d_chosen_method = h_methods[name];
 
-    auto F_a = F.packed_accessor<scalar_t, 2>();
-    auto x0_a = x0.packed_accessor<scalar_t, 1>();
-    auto g_a = g.packed_accessor<scalar_t, 1>();
+    auto F_a = F.packed_accessor<scalar_t, 2, torch::RestrictPtrTraits, size_t>();
+    auto x0_a = x0.packed_accessor<scalar_t, 1, torch::RestrictPtrTraits, size_t>();
+    auto g_a = g.packed_accessor<scalar_t, 1, torch::RestrictPtrTraits, size_t>();
 
     auto F_size = torch::size(F, 0);
     auto x0_size = torch::size(x0, 0);
@@ -135,13 +135,20 @@ torch::Tensor solve_cuda(torch::Tensor F, torch::Tensor x0, torch::Tensor g, flo
 
     switch(F_size) {
 	case 1:
-		compact_diagonal_solver<<<blocks, threadsPerBlock>>>(d_chosen_method, F_a, x0_a, g_a, dt, steps, x0_size);
+		AT_DISPATCH_FLOATING_TYPES(x0.type(), "solver_cuda", ([&] {
+			compact_diagonal_solver<scalar_t><<<blocks, threadsPerBlock>>>(d_chosen_method, F_a, x0_a, g_a, dt, steps, x0_size);
+		}));
 		break;
 	case 2:
-		compact_skew_symmetric_solver<<<blocks, threadsPerBlock>>>(d_chosen_method, F_a, x0_a, g_a, dt, steps, x0_size);
+		AT_DISPATCH_FLOATING_TYPES(x0.type(), "solver_cuda", ([&] {
+			compact_skew_symmetric_solver<scalar_t><<<blocks, threadsPerBlock>>>(d_chosen_method, F_a, x0_a, g_a, dt, steps, x0_size);
+		}));
 		break;
 	default:
-		general_solver<<<blocks, threadsPerBlock>>>(d_chosen_method, F_a, x0_a, g_a, dt, steps, x0_size);
+		AT_DISPATCH_FLOATING_TYPES(x0.type(), "solver_cuda", ([&] {
+			general_solver<scalar_t><<<blocks, threadsPerBlock>>>(
+				d_chosen_method, F_a, x0_a, g_a, dt, steps, x0_size);
+		}));
 		break;
     }
     
