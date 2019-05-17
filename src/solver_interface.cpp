b150af47e0511dad7dab53a60355679a951f82f1
Michael Poli <20184659@eelab35.kaist.ac.kr>
2019-05-17 13:55:50 +0900

    redirect

--- src/solver_interface.cpp
+++ src/solver_interface.cpp
@@ -25,5 +25,5 @@ void solve_cpp(torch::Tensor F, torch::Tensor x0, torch::Tensor g, double dt, in
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("solve", &solve_cpp, "ODE Solver (CUDA)");
+  m.def("solve", &solve_cudaa, "ODE Solver (CUDA)");
 }
