39b25a793a0d2869bb9bda8f75280d9640935f42
Michael Poli <20184659@eelab35.kaist.ac.kr>
2019-05-17 14:09:53 +0900

    null

--- src/solver_cuda.cu
+++ src/solver_cuda.cu
@@ -92,7 +92,7 @@ __device__ method_t p_euler_method = euler_method;
 __device__ method_t p_rk4_method = rk4_method;
 
 void solve_cudaa(torch::Tensor F, torch::Tensor x0, torch::Tensor g, float dt, int steps, string name){
-
+/*
     std::map<string, method_t> h_methods;
     method_t h_euler_method;
     method_t h_rk4_method; 
@@ -130,5 +130,6 @@ void solve_cudaa(torch::Tensor F, torch::Tensor x0, torch::Tensor g, float dt, i
 		//general_solver<<<blocks, threadsPerBlock>>>(d_chosen_method, F_a, x0_a, g_a, dt, steps, x0_size);
 		break;
     }
+*/
 }
 
